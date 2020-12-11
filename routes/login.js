75e3c995b9ed8dc266244588bd6be50e932dd005
Marios Venetsianos <mariosvenet@admin.grnet.gr>
2020-12-11 18:44:45 +0200

    Added more validation.

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,9 @@
 const routes = require('express').Router()
 let database = require('../services/database')
+const { body, check, validationResult } = require('express-validator');
 
-routes.get('/', async function(req,res,next){
+
+routes.get('/', async function (req, res, next) {
     /*let logoutuser = await database.user.findOne({where:{
         isLoggedIn: true
     }})
@@ -10,42 +12,61 @@ routes.get('/', async function(req,res,next){
         logoutuser.update({isLoggedIn: false})
         console.log(logoutuser)
         
-    }   */ 
- 
-    res.render("login")
+    }   */
+    req.session.username =null;
+    req.session.fname =null;
+    req.session.lname =null;
+    req.session.dikaiwmata_diaxeirisis =null;
+    req.session.rolos =null;
+    const valid_errors = req.session.errors;
+    req.session.errors = null;
+    res.render("login", { errors: valid_errors })
 });
 
-routes.post('/', async function(req,res,next){
+routes.post('/', [
+    // username must be an email
+    check("username")
+        .notEmpty().withMessage("Κενό όνομα χρήστη."),
+    // password must be at least 5 chars long
+    check("password")
+        .isLength({ min: 5 }).withMessage("Λανθασμένος κωδικός πρόσβασης.")
+        .notEmpty().withMessage("Κενός κωδικός πρόσβασης.")
+]
 
-    let user = await database.user.findOne({where:{
-        username: req.body.username,
-        password: req.body.password
-    }})
-    if(user && user.dataValues){
-        req.session.username = user.username;
-        req.session.fname = user.fname;
-        req.session.lname = user.lname;
-        req.session.dikaiwmata_diaxeirisis = user.dikaiwmata_diaxeirisis;
-        req.session.rolos = user.rolos;
-        console.log( "req.session.dikaiwmata_diaxeirisis: " + req.session.dikaiwmata_diaxeirisis
-                    + " , req.session.rolos: " + req.session.rolos)
-        if(user.dikaiwmata_diaxeirisis) {
-            res.send({redirect: "user_views/admin_dashboard"});
+    , async function (req, res, next) {
+        const errors = validationResult(req);
+        req.session.errors = errors.array();
+        if (errors.isEmpty()) {//no validation errors
+            let user = await database.user.findOne({//find a user with matching username & password
+                where: {
+                    username: req.body.username,
+                    password: req.body.password
+                }
+            })
+            if (user && user.dataValues) {//found a user
+                req.session.username = user.username;//store his data to session variables
+                req.session.fname = user.fname;
+                req.session.lname = user.lname;
+                req.session.dikaiwmata_diaxeirisis = user.dikaiwmata_diaxeirisis;
+                req.session.rolos = user.rolos;
+                // console.log( "req.session.dikaiwmata_diaxeirisis: " + req.session.dikaiwmata_diaxeirisis
+                //             + " , req.session.rolos: " + req.session.rolos)
+                if (user.dikaiwmata_diaxeirisis) {
+                    res.send({ redirect: "user_views/admin_dashboard" });//if user exists and has admin rights, redirect to admin dashboard
+                }
+                else {
+                    res.send({ redirect: "user_views/user_dashboard" });
+                }
+            } else {
+                req.session.errors.push({ msg: 'Δε βρέθηκε χρήστης με αυτό το όνομα ή κωδικό.' })//custom error message
+                res.send({ redirect: "./login" });//redirect and display errors
+            }
+        } else {
+            res.send({ redirect: "./login" });
         }
-        else { 
-            res.send({redirect: "user_views/user_dashboard"});
-        }
-    }else{
-        res.status(404).send("Not found")
-    }
-    console.log(req.sessionID)
-    console.log(req.session)
+        //console.log(req.sessionID)
+        //console.log(req.session)
 
-});
-
-/*this works. might be handy for put,delete. review later
-routes.get('/1', function(req,res,next){
-    res.render("create")
-});*/
+    });
 
 module.exports = routes;
