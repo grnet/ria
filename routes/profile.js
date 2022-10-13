78a87fff4aab5baf94ede04d84d9fed658dffde5
Marios Venetsianos <mariosvenet@admin.grnet.gr>
2022-10-13 16:37:53 +0300

    Fixed profile issues. Added pool options to database.js. Some fixes.

--- routes/profile.js
+++ routes/profile.js
@@ -27,54 +27,80 @@ routes.get('/', authUser, async function (req, res, next) {
 
 routes.put('/:username', authUser, async function (req, res, next) {
 
+    const password = req.body.password;
+    const newPassword = req.body.new_password;
+    const repeatPassword = req.body.password_repeat;
+
     req.session.errors = [];
     let user = await database.user.findOne({
-        where: {
-            username: req.params.username
-        }
+      where: {
+        username: req.params.username,
+      },
     });
     if (user && user.dataValues) {
-
-        bcrypt.compare(req.body.password, user.password, function (err, result) {
-            if (result) {
-                if (req.body.new_password === req.body.password_repeat) {
-                    bcrypt.hash(req.body.new_password, 10, async function (err, hash) {
-                        if (hash) {
-                            if (req.body.dikaiwmata_diaxeirisis) {
-                                await database.user.update({ fname: req.body.fname, lname: req.body.lname, username: req.body.username, password: hash, rolos: req.body.rolos, dikaiwmata_diaxeirisis: req.body.dikaiwmata_diaxeirisis, ypoyrgeio: req.body.ypoyrgeio },
-                                    {
-                                        where: {
-                                            username: req.params.username
-                                        }
-                                    });
-                                res.send({ redirect: "./dashboard" });
-                            } else {
-                                await database.user.update({ fname: req.body.fname, lname: req.body.lname, username: req.body.username, password: hash, rolos: req.body.rolos, dikaiwmata_diaxeirisis: '', ypoyrgeio: req.body.ypoyrgeio },
-                                    {
-                                        where: {
-                                            username: req.params.username
-                                        }
-                                    });
-                                res.send({ redirect: "./dashboard" });
-                            }
-                        }
-                    });
-                } else {
-                    req.session.errors.push({ msg: 'Οι νέοι κωδικοί δεν ταιριάζουν.' })//custom error message
-                    const errors = req.session.errors
-                    if (errors) { // if array exists
-                        console.log(errors);
-                        return res.status(422).json(errors);
-                    }
-                }
-            } else {
-                req.session.errors.push({ msg: 'Εισαγάγατε λανθασμένο κωδικό πρόσβασης.' });
-                res.send(req.session.errors);
+      if (!password) {
+        //if password not provided update everything but the password
+        await database.user
+          .update(
+            {
+              fname: req.body.fname,
+              lname: req.body.lname,
+              username: req.body.username,
+              rolos: req.body.rolos,
+              dikaiwmata_diaxeirisis: req.body.dikaiwmata_diaxeirisis,
+              ypoyrgeio: req.body.ypoyrgeio,
+            },
+            {
+              where: {
+                username: req.params.username,
+              },
             }
+          )
+          .then(res.send({ redirect: "./dashboard" }));
+      } else {
+        bcrypt.compare(password, user.password, function (err, result) {
+          //compare user passwords
+          if (result) {
+            if (newPassword === repeatPassword) {
+              bcrypt.hash(newPassword, 10, async function (err, hash) {
+                if (hash) {
+                  await database.user.update(
+                    {
+                      fname: req.body.fname,
+                      lname: req.body.lname,
+                      username: req.body.username,
+                      password: hash,
+                      rolos: req.body.rolos,
+                      dikaiwmata_diaxeirisis: req.body.dikaiwmata_diaxeirisis,
+                      ypoyrgeio: req.body.ypoyrgeio,
+                    },
+                    {
+                      where: {
+                        username: req.params.username,
+                      },
+                    }
+                  );
+                  res.send({ redirect: "./dashboard" });
+                }
+              });
+            } else {
+              req.session.errors.push({ msg: "Οι κωδικοί δεν ταιριάζουν." }); //custom error message
+              const errors = req.session.errors;
+              if (errors) {
+                return res.status(422).json(errors);
+              }
+            }
+          } else {
+            req.session.errors.push({
+              msg: "Εισαγάγατε λάθος κωδικό πρόσβασης.",
+            });
+            res.send(req.session.errors);
+          }
         });
+      }
     } else {
-        req.session.errors.push({ msg: 'Εισάγατε λανθασμένα στοιχεία.' })
-        return res.send(req.session.errors);
+      req.session.errors.push({ msg: "Εισαγάγατε λανθασμένα στοιχεία." });
+      return res.send(req.session.errors);
     }
 });
 
--- services/database.js
+++ services/database.js
@@ -1,8 +1,16 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const { applyAssoc } = require('../models/associations.js');
-const fs = require('fs');
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const { applyAssoc } = require("../models/associations.js");
+const fs = require("fs");
 
-const sequelize = new Sequelize({ database: process.env.DATABASE_NAME, username: process.env.DATABASE_USER, password: process.env.DATABASE_PASSWORD, dialect: process.env.DATABASE_DIALECT, host: process.env.DATABASE_HOST, port: process.env.DATABASE_PORT });
+const sequelize = new Sequelize({
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  dialect: process.env.DATABASE_DIALECT,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  pool: { max: 40, min: 0, acquire: 30000, idle: 10000 },
+});
 
 const db = {};
 
@@ -10,7 +18,10 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 //models
 db.ekthesi = require("../models/ekthesi.model.js")(sequelize, Sequelize);
-db.ekthesi_tables = require("../models/ekthesi_tables.model.js")(sequelize, Sequelize);
+db.ekthesi_tables = require("../models/ekthesi_tables.model.js")(
+  sequelize,
+  Sequelize
+);
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.audit = require("../models/audit.model.js")(sequelize, Sequelize);
 db.ministries = require("../models/ministries.model.js")(sequelize, Sequelize);
@@ -18,4 +29,3 @@ db.ministries = require("../models/ministries.model.js")(sequelize, Sequelize);
 applyAssoc(sequelize);
 
 module.exports = db;
-
