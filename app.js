28c39b63349afacb642bb0877add66a3b422a22b
Panagiotis Skarvelis <pskarvelis@admin.grnet.gr>
2022-12-12 18:20:32 +0200

    link to oauth2pa login form login page

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.disable('x-powered-by');
 app.use('/', homeRoute);
 app.use('/login', loginRoute);
 app.use(process.env.OAUTH2PA_LOGIN_PATH, oauth2pa); //Path have to be changed to /palogin or something, after KED is done
+app.use('/oauth2pa', oauth2pa);//this may not needed if the OAUTH2PA_LOGIN_PATH already starts with /oauth2pa
 app.use('/logout', logoutRoute);
 app.use('/create', createRoute);
 app.use('/form_a', form_aRoute);
