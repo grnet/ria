41384bd617b8fbd26148099c03651ce8ed4f98d4
Marios Venetsianos <mariosvenet@admin.grnet.gr>
2022-12-29 19:31:46 +0200

    Updated login to include oauth2pa. Better error control for login.

--- routes/login.js
+++ routes/login.js
@@ -1,53 +1,62 @@
-const routes = require('express').Router();
-let database = require('../services/database');
-const { body, check, validationResult } = require('express-validator');
-const bcrypt = require('bcrypt');
+const routes = require("express").Router();
+let database = require("../services/database");
+const { body, check, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const { user } = require("../services/database");
 
-routes.get('/', async function (req, res, next) {
-    const valid_errors = req.session.errors;
-    req.session.errors = null;
-    res.render("login", { errors: valid_errors })
+routes.get("/", async function (req, res, next) {
+  const valid_errors = req.session.errors;
+  req.session.errors = null;
+  res.render("login", { errors: valid_errors });
 });
 
-routes.post('/', [
-    // username must be an email
-    check("username")
-        .notEmpty().withMessage("Κενό όνομα χρήστη."),
-    // password must be at least 5 chars long
+routes.post(
+  "/",
+  [
+    check("username").notEmpty().withMessage("Κενό όνομα χρήστη."),
     check("password")
-        .isString().isLength({ min: 5 }).withMessage("Λανθασμένος κωδικός πρόσβασης.")
-        .notEmpty().withMessage("Κενός κωδικός πρόσβασης.")
-]
+      .isString()
+      .isLength({ min: 8 })
+      .withMessage(
+        "O κωδικός πρόσβασης πρέπει να περιέχει τουλάχιστον 8 χαρακτήρες."
+      )
+      .notEmpty()
+      .withMessage("Κενός κωδικός πρόσβασης."),
+  ],
 
-    , async function (req, res, next) {
-        const errors = validationResult(req);
-        req.session.errors = errors.array();
-        if (errors.isEmpty()) {//no validation errors
-            const userPassword = req.body.password;
-            let user = await database.user.findOne({//find a user with matching username & password
-                where: {
-                    username: req.body.username,
-                }
-            })
-            if (user && user.dataValues) {//found user
+  async function (req, res, next) {
+    const errors = validationResult(req);
 
-                bcrypt.compare(userPassword, user.password, function (err, result) {
-                    if (result) {
-                        req.session.user = user;
-                        res.send({ redirect: "user_views/dashboard" });                       
-                    } else {
-                        req.session.errors.push({ msg: 'Δε βρέθηκε χρήστης με αυτό το όνομα ή κωδικό.' })//custom error message
-                        res.send({ redirect: "./login" });//redirect and display errors
-                    }
-                })
-            } else {
-                req.session.errors.push({ msg: 'Δε βρέθηκε χρήστης με αυτό το όνομα ή κωδικό.' })//custom error message
-                res.send({ redirect: "./login" });//redirect and display errors
-            }
-        } else {
-            res.send({ redirect: "./login" });
-        }
-
-    });
+    if (errors.errors.length === 0) {
+      //no validation errors
+      const inputPassword = req.body.password;
+      let user = await database.user.findOne({
+        where: {
+          username: req.body.username,
+        },
+      });
+      if (user && user.dataValues) {
+        bcrypt.compare(inputPassword, user.password, function (err, result) {
+          if (result) {
+            req.session.user = user;
+            res.status(200).send({ redirect: "user_views/dashboard" });
+          } else {
+            errors.errors.push({
+              msg: "Δε βρέθηκε χρήστης με αυτό το όνομα ή κωδικό.",
+            });
+            res.status(404).send(errors.errors);
+          }
+        });
+      } else {
+        errors.errors.push({
+          msg: "Δε βρέθηκε χρήστης με αυτό το όνομα ή κωδικό.",
+        });
+        res.status(404).send(errors.errors);
+      }
+    } else {
+      res.status(400).send(errors.array());
+    }
+  }
+);
 
 module.exports = routes;
